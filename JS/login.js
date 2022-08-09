6e2f03e4dd00bdec7a92a7f6aad0f4cb687920e3
Llenc <costallenc@gmail.com>
2022-08-09 18:34:07 +0200

    login creado i avanzado

---
+++ JS/login.js
@@ -0,0 +1,7 @@
+function login(){
+    var userName = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+    if(password == "hola" && userName == "user"){
+        alert("furula");
+    }
+}
