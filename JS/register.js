5a695047c6141346c515683a8f8a405ae0bb7936
kevin <kevin@mail.esn>
2022-08-09 19:52:09 +0200

    creada func registro

--- JS/register.js
+++ JS/register.js
@@ -1,7 +1,34 @@
 //meto (surne,,){
+ var   userName
+ var   surname
+ var   password
+ var   email
+ var   fechaNacimiento
+ var   dni
+ var   telf
 
-
-RegExp(
-
+ function nombre(){
+    document.getElementById
+ }
+ function apellidos(){
+    document.getElementById
+ }
+ function pass(){
+    document.getElementById
+ }
+ function mail(){
+    document.getElementById
+ }
+ function nacimiento(){
+    document.getElementById
+ }
+ function idNumber(){
+    document.getElementById
+ }
+ function telf(){
+    document.getElementById
+ }
+RegExp{
+    (\+34|0034|34)?[]*(6|7)[]*([0-9][]*){8}
 }
 /*
