54bfcbac865a1a04979a80a9868b70a09820728a
Bryan Pibaque <bryan-alexiss1@hotmail.com>
2022-08-10 15:55:59 +0200

    Login hecho

--- JS/login.js
+++ JS/login.js
@@ -1,8 +1,111 @@
+var userName;
+var surname;
+var password;
+var email;
+var fechaNacimiento;
+var dni;
+var telf;
 
-function login(){
-    var userName = document.getElementById("user").value;
-    var password = document.getElementById("password").value;
-    if(password == "hola" && userName == "user"){
-        alert("furula");
-    }
+
+//Llamar a la funcion register del archivo register.js
+function obtenerDatos() {
+
+    $.getScript('./register.js',
+        v = register()
+    );
+
+    //Guardar variables
+    userName = v[0];
+    surname = v[1]
+    password = v[2];
+    email = v[3];
+    fechaNacimiento = v[4];
+    dni = v[5];
+    telf = v[6];
+
+    crearSesion()
+
 }
+
+//Crear localStorange
+function crearSesion() {
+
+    localStorage.setItem("user", userName)
+    localStorage.setItem("surname", surname)
+    localStorage.setItem("password", password)
+    localStorage.setItem("email", email)
+    localStorage.setItem("fechaNacimiento", fechaNacimiento)
+    localStorage.setItem("dni", dni)
+    localStorage.setItem("telf", telf)
+
+
+
+
+}
+
+//Elimina localStorage
+function cerrarSesion() {
+
+    localStorage.removeItem("user")
+    localStorage.removeItem("surname")
+    localStorage.removeItem("password")
+    localStorage.removeItem("email")
+    localStorage.removeItem("fechaNacimiento")
+    localStorage.removeItem("dni")
+    localStorage.removeItem("telf")
+
+    location.href = './login.html'
+}
+
+//Validar el form con el localStorage
+function validarLogin() {
+
+    if ($('#password').val() == localStorage.getItem("password") &&
+        $('#email').val() == localStorage.getItem("email")) {
+        alert('Credenciales correctas')
+
+        location.href = './modulos.html'
+
+
+    } else {
+        alert('Error de sesion!!!')
+    }
+
+}
+
+function cambiarNombre() {
+    $('#userLogin').text('Hola ' + localStorage.getItem("user"))
+}
+
+
+
+document.cookie = "nombre=oeschger";
+document.cookie = "comida_preferida=tripa";
+function alertCookie() {
+    alert(document.cookie); // visualizar: nombre=oeschger;comida favorita=tripa
+
+}
+
+
+
+
+
+
+
+
+/**/
+function mostrar() {
+    alert(localStorage.getItem("user"))
+    alert(localStorage.getItem("surname"))
+    alert(localStorage.getItem("password"))
+    alert(localStorage.getItem("email"))
+    alert(localStorage.getItem("fechaNacimiento"))
+    alert(localStorage.getItem("dni"))
+    alert(localStorage.getItem("telf"))
+
+
+
+}
+
+
+
--- JS/register.js
+++ JS/register.js
@@ -1,34 +1,29 @@
-//meto (surne,,){
- var   userName
- var   surname
- var   password
- var   email
- var   fechaNacimiento
- var   dni
- var   telf
+var userName;
+var surname;
+var password;
+var email;
+var fechaNacimiento;
+var dni;
+var telf;
 
- function nombre(){
-    document.getElementById
- }
- function apellidos(){
-    document.getElementById
- }
- function pass(){
-    document.getElementById
- }
- function mail(){
-    document.getElementById
- }
- function nacimiento(){
-    document.getElementById
- }
- function idNumber(){
-    document.getElementById
- }
- function telf(){
-    document.getElementById
- }
-RegExp{
-    (\+34|0034|34)?[]*(6|7)[]*([0-9][]*){8}
+function register() {
+    userName = $('#userName').val();
+    surname = $('#surname').val()
+    password = $('#password').val();
+    email = $('#email').val();
+    fechaNacimiento = $('#fechaNacimiento').val();
+    dni = $('#dni').val();
+    telf = $('#telf').val();
+
+    result = [userName,
+        surname,
+        password,
+        email,
+        fechaNacimiento,
+        dni,
+        telf]
+    console.log(result)
+    return result;
 }
-/*
+
+
